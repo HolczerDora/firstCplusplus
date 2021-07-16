3576a101bd51f2c6a5d1e1ae04ee0eacc1d8cf5c
Dorka <hdwdart@gmail.com>
2021-07-16 13:53:31 +0200

    a variable

--- string_osszehasonlitasa.cpp
+++ string_osszehasonlitasa.cpp
@@ -4,5 +4,8 @@ int main () {
     std::cout << (std::string("hello") == std::string("helló")) << std::endl;
     //távolság, üzemanyagár, fogyasztás
     std::cout << 600*6.5/100 << std::endl;
+    //declare to variable
+    int number = 9;
+    std::cout << number << std::endl;
 
 }
