a561ec9432b07026f265bd458969ff91d669b4f2
Dorka <hdwdart@gmail.com>
2021-07-16 08:31:33 +0200

    auto,km,price

--- string_osszehasonlitasa.cpp
+++ string_osszehasonlitasa.cpp
@@ -2,4 +2,7 @@
 int main () {
     std::cout << (std::string("hello") == std::string("hello")) << std::endl;
     std::cout << (std::string("hello") == std::string("helló")) << std::endl;
+    //távolság, üzemanyagár, fogyasztás
+    std::cout << 600*6.5/100 << std::endl;
+
 }
