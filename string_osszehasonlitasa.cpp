89b13eb56df8a69709791df75c9d1514ee363994
Dorka <hdwdart@gmail.com>
2021-07-16 07:34:19 +0200

    add changes

--- string_osszehasonlitasa.cpp
+++ string_osszehasonlitasa.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
 int main () {
     std::cout << (std::string("hello") == std::string("hello")) << std::endl;
+    std::cout << (std::string("hello") == std::string("helló")) << std::endl;
 }
