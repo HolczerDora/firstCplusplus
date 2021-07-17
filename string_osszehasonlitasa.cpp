12dd447fdce99aa696ba0bbc03e1760cf7ed0c1f
Dorka <hdwdart@gmail.com>
2021-07-17 14:15:09 +0200

    game with variable

--- string_osszehasonlitasa.cpp
+++ string_osszehasonlitasa.cpp
@@ -14,4 +14,13 @@ int main () {
     std::string user_address;
     std::string phone_number;
     std::cout << user_name << std::endl;
+    //game with variables
+    int sum_of_the_two_number = 10+10; 
+    std::cout << sum_of_the_two_number <<std::endl;
+
+    std::string my_name = "Dorka";
+    std::cout << "My name is: "<< my_name << std::endl;
+  
+    int ultimate_answer =1890/90*2;
+    std::cout << "The answer to life, \nthe universe, \nand everything: " << ultimate_answer << std::endl;
 }
